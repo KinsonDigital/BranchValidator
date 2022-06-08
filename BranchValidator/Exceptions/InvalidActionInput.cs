f9464718688bd776811f520b6d44ed7a093d03cf
Calvin Wilkinson <kinsondigital@gmail.com>
2022-06-08 14:17:59 +0100

    Create new exception for invalid action input values

---
+++ BranchValidator/Exceptions/InvalidActionInput.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="InvalidActionInput.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace BranchValidator.Exceptions;
+
+/// <summary>
+/// Occurs when a GitHub action input has an invalid value.
+/// </summary>
+public class InvalidActionInput : Exception
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidActionInput"/> class.
+    /// </summary>
+    public InvalidActionInput()
+        : base("The action input value is invalid.") => HResult = 10;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidActionInput"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    public InvalidActionInput(string message)
+        : base(message) => HResult = 10;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidActionInput"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="innerException">
+    ///     The <see cref="Exception"/> instance that caused the current exception.
+    /// </param>
+    public InvalidActionInput(string message, Exception innerException)
+        : base(message, innerException) => HResult = 10;
+}
---
+++ Testing/BranchValidatorTests/Exceptions/InvalidActionInputTests.cs
@@ -0,0 +1,54 @@
+﻿// <copyright file="InvalidActionInputTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using BranchValidator.Exceptions;
+using FluentAssertions;
+
+namespace BranchValidatorTests.Exceptions;
+
+/// <summary>
+/// Tests the <see cref="InvalidActionInput"/> class.
+/// </summary>
+public class InvalidActionInputTests
+{
+    #region Constructor Tests
+    [Fact]
+    public void Ctor_WithNoParam_CorrectlySetsExceptionMessage()
+    {
+        // Act
+        var exception = new InvalidActionInput();
+
+        // Assert
+        exception.Message.Should().Be("The action input value is invalid.");
+        exception.HResult.Should().Be(10);
+    }
+
+    [Fact]
+    public void Ctor_WhenInvokedWithSingleMessageParam_CorrectlySetsMessage()
+    {
+        // Act
+        var exception = new InvalidActionInput("test-message");
+
+        // Assert
+        exception.Message.Should().Be("test-message");
+        exception.HResult.Should().Be(10);
+    }
+
+    [Fact]
+    public void Ctor_WhenInvokedWithMessageAndInnerException_ThrowsException()
+    {
+        // Arrange
+        var innerException = new Exception("inner-exception");
+
+        // Act
+        var deviceException = new InvalidActionInput("test-exception", innerException);
+
+        // Assert
+        deviceException.InnerException.Message.Should().Be("inner-exception");
+        deviceException.Message.Should().Be("test-exception");
+        deviceException.HResult.Should().Be(10);
+    }
+    #endregion
+}
+
