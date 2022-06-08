d07b4a102d6e9152e898658cdcbe3d9ff702e62c
Calvin Wilkinson <kinsondigital@gmail.com>
2022-06-08 22:27:19 +0100

    Create service to perform JSON serialization

---
+++ BranchValidator/Services/JSONService.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="JSONService.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BranchValidator.Services;
+
+/// <inheritdoc/>
+public class JSONService : IJSONService
+{
+    private readonly JsonSerializerOptions options;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="JSONService"/> class.
+    /// </summary>
+    public JSONService()
+    {
+        this.options = new JsonSerializerOptions { WriteIndented = true };
+        this.options.Converters.Add(new JsonStringEnumConverter());
+    }
+
+    /// <inheritdoc/>
+    public string Serialize(object? value) => JsonSerializer.Serialize(value, this.options);
+
+    /// <inheritdoc/>
+    public T? Deserialize<T>(string value)
+    {
+        using var stream = new MemoryStream(Encoding.UTF8.GetBytes(value));
+        return JsonSerializer.Deserialize<T>(stream, this.options);
+    }
+}
