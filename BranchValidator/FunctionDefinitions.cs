330246cc523fe9b0c7ca29f964ae04b71008f09f
Calvin Wilkinson <kinsondigital@gmail.com>
2022-06-08 22:53:06 +0100

    Create new function definitions class to hold all of available expression functions that a user can use

---
+++ BranchValidator/FunctionDefinitions.cs
@@ -0,0 +1,36 @@
+﻿// <copyright file="FunctionDefinitions.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace BranchValidator;
+
+/// <inheritdoc/>
+public class FunctionDefinitions : IFunctionDefinitions
+{
+    private static readonly char[] Numbers =
+    {
+        '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    };
+
+    /// <inheritdoc/>
+    public bool EqualTo(string value, string branchName)
+    {
+        if (string.IsNullOrEmpty(value) && string.IsNullOrEmpty(branchName))
+        {
+            return true;
+        }
+
+        return value == branchName;
+    }
+
+    /// <inheritdoc/>
+    public bool IsCharNum(uint charPos, string branchName)
+    {
+        if (string.IsNullOrEmpty(branchName))
+        {
+            return false;
+        }
+
+        return charPos <= branchName.Length - 1 && Numbers.Contains(branchName[(int)charPos]);
+    }
+}
---
+++ BranchValidator/IFunctionDefinitions.cs
@@ -0,0 +1,44 @@
+﻿// <copyright file="IFunctionDefinitions.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+namespace BranchValidator;
+
+/// <summary>
+/// The available functions that can be used in the GitHub action validation logic expression.
+/// </summary>
+public interface IFunctionDefinitions
+{
+    /// <summary>
+    /// Returns a value indicating whether or not a branch with the given <paramref name="branchName"/>
+    /// matches the given <paramref name="value"/>.
+    /// </summary>
+    /// <param name="value">The value to check against the <paramref name="branchName"/>.</param>
+    /// <param name="branchName">The name of the branch.</param>
+    /// <returns><c>true</c> if the <paramref name="value"/> is equal to the <paramref name="branchName"/>.</returns>
+    /// <remarks>
+    ///     The comparison is case sensitive.
+    /// </remarks>
+    bool EqualTo(string value, string branchName);
+
+    /// <summary>
+    /// Returns a value indicating whether or not the <c>char</c> at the given <paramref name="charPos"/>
+    /// is a number in a branch <c>string</c> that matches the given <paramref name="branchName"/>.
+    /// </summary>
+    /// <param name="charPos">The position of the character.</param>
+    /// <param name="branchName">The name of the branch.</param>
+    /// <returns><c>true</c> if the character is a number.</returns>
+    /// <remarks>
+    /// Things to consider:
+    /// <list type="bullet">
+    ///     <item>
+    ///         If the character position is larger than the length of the <paramref name="branchName"/>,
+    ///         then the result will automatically be <c>false</c>.
+    ///     </item>
+    ///     <item>
+    ///         A null or empty branch name will return <c>false.</c>
+    ///     </item>
+    /// </list>
+    /// </remarks>
+    bool IsCharNum(uint charPos, string branchName);
+}
