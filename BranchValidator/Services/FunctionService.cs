4ea09a01d781bc20f3190f139e6a90077c599dbf
Calvin Wilkinson <kinsondigital@gmail.com>
2022-06-08 14:19:15 +0100

    Adjust the function data to contain the function name and param name list instead of param count

--- BranchValidator/Services/FunctionService.cs
+++ BranchValidator/Services/FunctionService.cs
@@ -14,12 +14,17 @@ public class FunctionService : IFunctionService
     {
         '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
     };
+    private static readonly char[] LowerCaseLetters =
+    {
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    };
 
     // TODO: Possibly load this data as JSON data from an embedded JSON data file
-    private readonly Dictionary<string, DataTypes> validFunctions = new ()
+    private readonly Dictionary<string, DataTypes[]> validFunctions = new ()
     {
-        { "equalTo:1", DataTypes.String },
-        { "isCharNum:1", DataTypes.Number },
+        // NOTE: For no parameters, just leave the right side of the ':' empty.  Example: equalTo:
+        { "equalTo:value", new[] { DataTypes.String } },
+        { "isCharNum:charPos", new[] { DataTypes.Number } },
     };
     private readonly IMethodExecutor methodExecutor;
 
@@ -43,19 +48,34 @@ public class FunctionService : IFunctionService
             throw new Exception("Valid function data set key missing a ':' character.");
         }
 
+        // Check that the param names section after the function name is valid
         if (this.validFunctions.Keys.Any(k =>
             {
                 var sections = k.Split(':', StringSplitOptions.RemoveEmptyEntries);
 
-                if (sections.Length <= 1 || sections[1].Any(c => Numbers.DoesNotContain(c)))
-                {
-                    return true;
-                }
-
-                return false;
+                // Commas aloud but param names only allowed to have lower and upper case letters
+                return sections.Length <= 1 || sections[1].Any(c => c != ',' && LowerCaseLetters.DoesNotContain(c.ToString().ToLower()[0]));
             }))
         {
             throw new Exception("Valid function data set key does not end with a number.");
+        }
+
+        // Check if the param name count is equal to the param data list count
+        foreach (KeyValuePair<string, DataTypes[]> validFunction in this.validFunctions)
+        {
+            var functionSections = validFunction.Key.Split(':', StringSplitOptions.RemoveEmptyEntries);
+            var paramNames = functionSections.Length >= 2
+                ? functionSections[1].Split(',', StringSplitOptions.RemoveEmptyEntries)
+                : Array.Empty<string>();
+
+            // If no parameters exist, move on
+            if (paramNames.Length <= 0 || paramNames.Length == validFunction.Value.Length)
+            {
+                continue;
+            }
+
+            var exceptionMsg = $"The total number of parameter names vs the total number of parameter data types do not match for function '{functionSections[0]}'.";
+            throw new Exception(exceptionMsg);
         }
 
         AvailableFunctions = this.validFunctions.Select(f =>
@@ -90,10 +110,8 @@ public class FunctionService : IFunctionService
             throw new ArgumentNullException(nameof(functionName), "The parameter must not be null or empty.");
         }
 
-        var fullFuncName = $"{functionName}:{paramPos}";
-
         var foundFunction = (from f in this.validFunctions
-            where f.Key == fullFuncName
+            where f.Key.StartsWith(functionName)
             select f).ToArray();
 
         if (foundFunction is null || foundFunction.Length <= 0)
@@ -101,7 +119,7 @@ public class FunctionService : IFunctionService
             throw new Exception($"The function '{functionName}' was not found.");
         }
 
-        return foundFunction[0].Value;
+        return foundFunction[0].Value[paramPos - 1];
     }
 
     /// <inheritdoc/>
