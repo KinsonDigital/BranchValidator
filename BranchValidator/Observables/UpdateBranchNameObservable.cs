5fc0ab687bfdb0e42ac9764efa26301412813aab
Calvin Wilkinson <kinsondigital@gmail.com>
2022-06-09 12:34:07 +0100

    Create observable to perform branch name updates

---
+++ BranchValidator/Observables/UpdateBranchNameObservable.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="UpdateBranchNameObservable.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using BranchValidator.Observables.Core;
+
+namespace BranchValidator.Observables;
+
+/// <summary>
+/// Creates an <see cref="Core.IObservable{T}"/> to send push notifications to signal that the position of the mouse has changed.
+/// </summary>
+public class UpdateBranchNameObservable : Observable<string>
+{
+    /// <summary>
+    /// Sends a push notification to update the name of a branch string.
+    /// </summary>
+    /// <param name="data">The data to send with the push notification.</param>
+    public override void PushNotification(string data)
+    {
+        for (var i = Observers.Count - 1; i >= 0; i--)
+        {
+            Observers[i].OnNext(data);
+        }
+    }
+}
---
+++ Testing/BranchValidatorTests/Observables/UpdateBranchNameObservableTests.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file="UpdateBranchNameObservableTests.cs" company="KinsonDigital">
+// Copyright (c) KinsonDigital. All rights reserved.
+// </copyright>
+
+using BranchValidator.Observables;
+using Moq;
+
+namespace BranchValidatorTests.Observables;
+
+/// <summary>
+/// Tests the <see cref="UpdateBranchNameObservable"/> class.
+/// </summary>
+public class UpdateBranchNameObservableTests
+{
+    #region Method Tests
+    [Fact]
+    public void PushNotification_WhenInvoked_SendsPushNotification()
+    {
+        // Arrange
+        // var observer = new Mock<CoreObserver>();
+        var observer = new Mock<IStringObserver>();
+
+        var observable = new UpdateBranchNameObservable();
+        observable.Subscribe(observer.Object);
+
+        var data = "test-data";
+
+        // Act
+        observable.PushNotification(data);
+
+        // Assert
+        observer.Verify(m => m.OnNext(data), Times.Once());
+    }
+    #endregion
+}
