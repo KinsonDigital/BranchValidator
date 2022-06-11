8b8a56c369b6de0f7156b3d40eebd669bc58fe18
Calvin Wilkinson <kinsondigital@gmail.com>
2022-06-11 11:20:00 +0100

    Create basic script generator project

---
+++ Tooling/ScriptGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ReSharper disable once InconsistentNaming
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ScriptGenerator;
+using ScriptGenerator.Services;
+
+// var basePath = $@"K:\SOFTWARE DEVELOPMENT\PERSONAL\BranchValidator\Tooling\";
+// var fileName = $"Hello{data}.txt";
+// var fullPath = $"{basePath}{fileName}";
+
+IHost host = null!;
+
+host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+    {
+        services.AddSingleton<IArgParsingService<AppInputs>, ArgParsingService>();
+        services.AddSingleton<IFileLoaderService<string>, TextualFileLoader>();
+    }).Build();
+
+
+var argParsingService = host.Services.GetRequiredService<IArgParsingService<AppInputs>>();
+
+await argParsingService.ParseArguments(
+    new AppInputs(),
+    args,
+    async inputs =>
+    {
+    }, errors =>
+    {
+    });
